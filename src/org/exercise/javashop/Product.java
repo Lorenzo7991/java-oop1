949298c6644d90ddcc5dbf6b30291d06decb17d7
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-17 14:39:38 +0200

    Add random method to randomize productCode. Add basic setter and getter methods

--- src/org/exercise/javashop/Product.java
+++ src/org/exercise/javashop/Product.java
@@ -1,4 +1,6 @@
 package org.exercise.javashop;
+// Import java util random
+import java.util.Random;
 
 public class Product {
     // Atributes
@@ -10,10 +12,46 @@ public class Product {
 
     // Builder
     public Product(String name, int productCode, String description, double price, double vat) {
+        // Assignment of the random object to create the product code as randomized number
+        this.productCode = new Random().nextInt(1000000);
         this.name = name;
-        this.productCode = productCode;
         this.description = description;
         this.price = price;
         this.vat = vat;
     }
+
+    // Getter and Setter methods
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public double getVat(){
+        return vat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setVat(double vat) {
+        this.vat = vat;
+    }
+
+
 }
